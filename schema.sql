07499af0ad2fa43b5572619c71a7399ddea4c3f0
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-01-11 07:52:15 +0300

    Update queries.sql file

--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals (
-vet_clinic(# id BIGSERIAL NOT NULL PRIMARY KEY,
-vet_clinic(# name VARCHAR(50) NOT NULL,
-vet_clinic(# date_of_birth DATE NOT NULL,
-vet_clinic(# escape_attempts INT,
-vet_clinic(# neutered BOOLEAN,
-vet_clinic(# weight_kg DECIMAL NOT NULL 
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL NOT NULL 
 );
