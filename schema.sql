31c9a21dd313b5923015dc03b840025cfb74677f
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-01-13 21:28:48 +0300

    Update the schema.sql file

--- schema.sql
+++ schema.sql
@@ -47,3 +47,27 @@ ALTER TABLE animals
     ADD CONSTRAINT owners_id 
     FOREIGN KEY(owners_id) 
     REFERENCES owners(id);
+
+-- Many to many relationships
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    species_id INT,
+    vets_id INT,
+    CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+    animals_id INT,
+    vets_id INT,
+    date_of_visit DATE,
+    CONSTRAINT fk_animals FOREIGN KEY(animals_id) REFERENCES animals(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
